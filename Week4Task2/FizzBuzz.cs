eb18b860e945439aed55766f2619df95438901a5
Urszula Goliszewska <urszula.goliszewska@gmail.com>
2023-11-07 20:42:34 +0100

    Add Project

---
+++ Week4Task2/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Task2
+{
+    class FizzBuzz
+    {
+        public string ResultGame(int input)
+        {
+            if(input % 3 == 0 && input % 5 == 0)
+            {
+                return "FizzBuzz";
+
+            }else if( input%3 == 0){
+
+                return "Fizz";
+
+            }else if ( input%5 == 0)
+            {
+                return "Buzz";
+            }
+            return input.ToString();
+        }
+    }
+}
---
+++ Week4Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in our FizzBuzz Game!\n");
+            Console.WriteLine("Type your number: \n");
+
+            while (true)
+            {              
+                var yourNumber = GetInput();
+                var fizzBuzz = new FizzBuzz();
+  
+                var result = fizzBuzz.ResultGame(yourNumber);
+                Console.WriteLine(result);
+                Console.WriteLine("Type your number, again or type 'q' to quit game");                  
+            }
+        }
+
+        private static int GetInput()
+        {
+            while (true)
+            {
+                var value = Console.ReadLine();
+
+                if(value.ToUpper() == "Q")
+                {
+                    Environment.Exit(0);
+                }
+
+                if(!int.TryParse(value, out int input))
+                {       
+                    Console.WriteLine("Wrong value. Type your number, please: \n");
+                    continue;
+                }
+                return input;                
+            }
+        }
+    }
+}
